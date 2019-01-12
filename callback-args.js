9f7bd71aaf6bfe92ed8a4d4541b232a3ebbd5f41
Tim Williams <timwilliams.txW@gmail.com>
2019-01-12 21:28:29 +0000

    refactor find waldo with foreach

--- callback-args.js
+++ callback-args.js
@@ -1,11 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-
+  arr.forEach(function(element) {
+    if (element === "Waldo") {
+      found(arr.indexOf(element));   // execute callback
     }
-  }
+  })
 }
 
 function actionWhenFound(index) {
